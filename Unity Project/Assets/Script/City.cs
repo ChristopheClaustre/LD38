2f0935a8663a817af82f24b65b1edd57f4cb629c
Syn4ck <xavier.chalut@gmail.com>
2017-04-22 14:54:31 +0200

    Ajout de classes
    Classes Building + City + Planet

---
+++ Unity Project/Assets/Script/City.cs
@@ -0,0 +1,98 @@
+﻿/***************************************************
+ *** INCLUDE                ************************
+ ***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************
+ *** THE CLASS              ************************
+ ***************************************************/
+
+
+public class City : MonoBehaviour
+{
+    /***************************************************
+	 ***  UNITY GUI PROPERTY    ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  SUB-CLASSES           ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+    public Camera camera;
+    public Building[] buildings;
+    public int population;
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  ATTRIBUTES            ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  METHODS               ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    // Use this for initialization
+    public void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+    public int getProductionPollution(double deltaTime)
+    {
+
+    }
+    public int getProductionArgent(double deltaTime)
+    {
+
+    }
+    public int getConsomationEau(double deltaTime)
+    {
+
+    }
+    public int getConsomationEnergie(double deltaTime)
+    {
+
+    }
+    public int getVent()
+    {
+
+    }
+    public int getEnsoleillement()
+    {
+
+    }
+    public float getSatisfaction()
+    {
+
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+}
---
+++ Unity Project/Assets/Script/Planet.cs
@@ -0,0 +1,96 @@
+﻿/***************************************************
+ *** INCLUDE                ************************
+ ***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************
+ *** THE CLASS              ************************
+ ***************************************************/
+
+
+public class Planet : MonoBehaviour
+{
+
+    /***************************************************
+	 ***  UNITY GUI PROPERTY    ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  SUB-CLASSES           ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+    public City[] cities;
+    public int water;
+    public int coal;
+    public int energy;
+    public int pollution;
+    public int time;
+    public int timeUnit;
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  ATTRIBUTES            ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    /***************************************************
+	 ***  METHODS               ************************
+	 ***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    // Use this for initialization
+    public void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+    public void updatePollution(double deltaTime)
+    {
+
+    }
+    public void updateEau(double deltaTime)
+    {
+
+    }
+    public void updateCharbon(double deltaTime)
+    {
+
+    }
+    public int getPopulation()
+    {
+
+    }
+    public float getVitesseRotation()
+    {
+
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+}
